d662d3d0b05b7ecdf0ced337cfe0a45269c42cc1
aluyapeter <aluyapeter7@gmail.com>
2026-01-10 11:23:09 +0100

    changed

--- app/tasks.py
+++ app/tasks.py
@@ -10,7 +10,6 @@ def send_email_task(email_to: str, subject: str, html_content: str):
     Sends an email using Gmail SMTP via a background worker.
     """
     try:
-        # 1. Load Config
         smtp_server = os.getenv("SMTP_SERVER")
         smtp_port = int(os.getenv("SMTP_PORT", 587))
         smtp_user = os.getenv("SMTP_USERNAME")
@@ -18,16 +17,13 @@ def send_email_task(email_to: str, subject: str, html_content: str):
         sender_email = os.getenv("EMAILS_FROM_EMAIL")
         sender_name = os.getenv("EMAILS_FROM_NAME")
 
-        # 2. Build the Email
         message = MIMEMultipart()
         message["From"] = f"{sender_name} <{sender_email}>"
         message["To"] = email_to
         message["Subject"] = subject
 
-        # Attach HTML content
         message.attach(MIMEText(html_content, "html"))
 
-        # 3. Connect to Gmail & Send
         print(f"Connecting to Gmail for {email_to}...")
         with smtplib.SMTP(smtp_server, smtp_port) as server:
             server.starttls() # Secure the connection (Encrypts the line)
